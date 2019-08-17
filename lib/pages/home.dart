2630a406210fcb2ffbc9cbfeff007c7f7740295f
Clayton Gilmer <netsec_planes@outlook.com>
2019-08-17 14:51:39 -0400

    multidex, mockup homepage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,14 +11,29 @@ class HomePage extends StatelessWidget {
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text('Clayton Gilmer'),
+              accountEmail: Text('netsecplanes@gmail.com'),
+            ),
             ListTile(
               title: Text('Matches'),
+              leading: Icon(Icons.calendar_today),
             ),
             ListTile(
               title: Text('Teams'),
+              leading: Icon(Icons.people),
             ),
             ListTile(
-              title: Text('Pick List'),
+              title: Text('First Pick'),
+              leading: Icon(Icons.looks_one),
+            ),
+            ListTile(
+              title: Text('Second Pick'),
+              leading: Icon(Icons.looks_two),
+            ),
+            ListTile(
+              title: Text('Settings'),
+              leading: Icon(Icons.settings),
             )
           ],
         ),
@@ -29,8 +44,20 @@ class HomePage extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 ListTile(
-                  title: Text('Scout a new match'),
-                  subtitle: Text('Scout a new match'),
+                  title: Text('Scout a match'),
+                  subtitle: Text('Start scouting a new match.'),
+                  leading: Icon(Icons.gamepad),
+                )
+              ],
+            ),
+          ),
+          Card(
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text('Scout a pit'),
+                  subtitle: Text('Start scouting a team\'s pit.'),
+                  leading: Icon(Icons.flag),
                 )
               ],
             ),
