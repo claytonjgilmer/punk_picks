bd9a52b8574a1667825c053ee800fb53b26edcb9
Clayton Gilmer <netsec_planes@outlook.com>
2019-08-22 00:35:59 -0400

    initial changes implementing flutter_form_builder

--- lib/pages/forms/match_scout_2019.dart
+++ lib/pages/forms/match_scout_2019.dart
@@ -1,146 +1,116 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:punk_picks/models/model_match_2019.dart';
-import 'package:punk_picks/components/counter_form_field.dart';
-import 'package:punk_picks/components/number_form_field.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 class MatchScoutPage extends StatefulWidget {
   _MatchScoutPageState createState() => _MatchScoutPageState();
 }
 
 class _MatchScoutPageState extends State<MatchScoutPage> {
-  final formKey = GlobalKey<FormState>();
-  ModelMatch2019 matchData;
-
-  void initState() {
-    super.initState();
-    // Set default match and game states
-    matchData = ModelMatch2019(
-      scoutName: '',
-      matchType: 'q',
-      matchNumber: 1,
-      allianceColor: 'r',
-      scoutNotes: '',
-      sandstormHabLevel: 1,
-      sandstormHabSuccess: false,
-      hatchesDuringSandstorm: 0,
-      cargoDuringSandstorm: 0,
-      hatchesDuringTeleop: 0,
-      cargoDuringTeleop: 0,
-      endgameHabLevel: 1,
-      climbRP: false,
-      rocketRP: false,
-    );
-  }
+  final GlobalKey<FormBuilderState> formKey = GlobalKey<FormBuilderState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Match Scout Form')
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(40),
-        child: Form(
-          key: formKey,
-          child: Column(
-            children: <Widget>[
-              // Begin game-agnostic form section
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Scout Name'),
-                onSaved: (String value) => matchData.scoutName = value,
-              ),
-              DropdownButtonFormField<String>(
-                value: matchData.matchType,
-                onChanged: (String value) {
-                  setState(() {
-                   matchData.matchType = value; 
-                  });
-                },
-                decoration: InputDecoration(labelText: 'Match Type'),
-                items: <DropdownMenuItem<String>>[
-                  DropdownMenuItem<String>(
-                    value: 'q',
-                    child: Text('Qualification'),
-                  ),
-                  DropdownMenuItem<String>(
-                    value: 'qf',
-                    child: Text('Quarterfinal'),
-                  ),
-                  DropdownMenuItem<String>(
-                    value: 'sf',
-                    child: Text('Semifinal'),
-                  ),
-                  DropdownMenuItem<String>(
-                    value: 'f',
-                    child: Text('Final'),
-                  )
-                ],
-              ),
-              NumberFormField(
-                decoration: InputDecoration(labelText: 'Match Number'),
-                onSaved: (int value) => matchData.matchNumber = value,
-              ),
-              NumberFormField(
-                decoration: InputDecoration(labelText: 'Team Number'),
-                onSaved: (int value) => matchData.teamNumber = value,
-              ),
-              DropdownButtonFormField<String>(
-                value: matchData.allianceColor,
-                onChanged: (String value) {
-                  setState(() {
-                    matchData.allianceColor = value;
-                  });
-                },
-                decoration: InputDecoration(labelText: 'Alliance Color'),
-                items: <DropdownMenuItem<String>>[
-                  DropdownMenuItem<String>(
-                    value: 'r',
-                    child: Text('Red'),
-                  ),
-                  DropdownMenuItem<String>(
-                    value: 'b',
-                    child: Text('Blue'),
-                  )
-                ],
-              ),
-              // End game-agnostic form section
-              DropdownButtonFormField(
-                value: matchData.sandstormHabLevel,
-                onChanged: (int value) {
-                  setState(() {
-                   matchData.sandstormHabLevel = value; 
-                  });
-                },
-                decoration: InputDecoration(labelText: 'Starting HAB level'),
-                items: <DropdownMenuItem<int>>[
-                  DropdownMenuItem<int>(
-                    value: 1,
-                    child: Text('Level 1'),
-                  ),
-                  DropdownMenuItem<int>(
-                    value: 2,
-                    child: Text('Level 2'),
-                  )
-                ],
-              ),
-              CheckboxListTile(
-                title: Text('Successfully left HAB platform'),
-                value: matchData.sandstormHabSuccess,
-                onChanged: (bool value) {
-                  setState(() {
-                    matchData.sandstormHabSuccess = value;
-                  });
-                },
-              ),
-              CounterFormField(
-                title: 'Hatches during sandstorm'
-                
-              )
-            ],
+      appBar: AppBar(title: Text('Match Scout Form'),),
+      body: SingleChildScrollView( 
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: FormBuilder(
+            key: formKey,
+            autovalidate: true,
+            initialValue: {
+              'scoutName' : '',
+              'matchType' : 'q',
+              // HACK: matchNumber and teamNumber attributes don't like being declared as int (FormBuilderTextView throws an error 
+              // due to expecting a string), so commenting out and letting flutter infer datatype
+              //'matchNumber' : 0,
+              //'teamNumber' : 0,
+              'sandstormHabLevel' : 1,
+              'sandstormHabSuccess' : false,
+              'cargoScoredL1' : 0,
+              'cargoScoredL2' : 0,
+              'cargoScoredL3' : 0,
+              'hatchScoredL1' : 0,
+              'hatchScoredL2' : 0,
+              'hatchScoredL3' : 0,
+              'endgameHabLevel' : 1,
+              'endgameHabSuccess' : false,
+              'rocketRP' : false,
+              'climbRP' : false,
+              'scoutNotes' : '',
+            },
+            child: Column(
+              children: <Widget>[
+                FormBuilderTextField(
+                  attribute: 'scoutName',
+                  decoration: InputDecoration(labelText: 'Scout Name'),
+                  validators: [
+                    FormBuilderValidators.required(),
+                  ],
+                ),
+                FormBuilderDropdown(
+                  attribute: 'matchType',
+                  decoration: InputDecoration(labelText: 'Match Type'),
+                  items: [
+                    DropdownMenuItem(
+                      value: 'q',
+                      child: Text('Qualification'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'qf',
+                      child: Text('Quarterfinal'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'sf',
+                      child: Text('Semifinal'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'f',
+                      child: Text('Final'),
+                    )
+                  ],
+                ),
+                FormBuilderTextField(
+                  attribute: 'matchNumber',
+                  decoration: InputDecoration(labelText: 'Match Number'),
+                  valueTransformer: (text) => num.tryParse(text),
+                  validators: [
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.numeric(),
+                    FormBuilderValidators.maxLength(2),
+                  ],
+                ),
+                FormBuilderTextField(
+                  attribute: 'teamNumber',
+                  decoration: InputDecoration(labelText: 'Team Number'),
+                  valueTransformer: (text) => num.tryParse(text),
+                  validators: [
+                    FormBuilderValidators.required(),
+                    FormBuilderValidators.numeric(),
+                    FormBuilderValidators.maxLength(4),
+                  ],
+                ),
+                FormBuilderRadio(
+                  attribute: 'sandstormHabLevel',
+                  decoration: InputDecoration(labelText: 'Starting HAB level'),
+                  options: [
+                    FormBuilderFieldOption(
+                      value: 1,
+                    ),
+                    FormBuilderFieldOption(
+                      value: 2,
+                    ),
+                    FormBuilderFieldOption(
+                      value: 3,
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
-      )
+      ),
     );
   }
 }
